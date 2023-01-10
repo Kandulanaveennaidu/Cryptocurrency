bedfb9e3d88d2fc46d3bb5660f2ba139d1401888
Kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-01-10 14:04:17 +0530

    first.commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+
+import React from "react";
+
+const avatarSrc =
+  "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text color={"skyblue"} fontFamily={"aerial"}>
+            {" "}
+            Founder: Kandula Naveen
+          </Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
